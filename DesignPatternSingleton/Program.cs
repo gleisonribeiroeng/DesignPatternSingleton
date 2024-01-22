1678c7455817a02d157ef77d6050e25cc2e04bd6
XMrGX <gleison423200@gmail.com>
2024-01-22 09:15:38 -0300

    Add project files.

---
+++ DesignPatternSingleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using DesignPatternSingleton;
+using System;
+
+Thread t1 = new Thread(() => {
+    var instance = UploadService.Instance(1);
+});
+
+Thread t2 = new Thread(() => {
+    var instance = UploadService.Instance(2);
+});
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
---
+++ DesignPatternSingleton/UploadService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternSingleton
+{
+    sealed class UploadService
+    {
+        private UploadService(){ }
+
+        public int Id { get; private set; }
+
+        private static UploadService _instance;
+
+        private static readonly object _lock = new object();
+
+        public static UploadService Instance(int id)
+        {
+            if(_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new UploadService();
+                        _instance.Id = id;
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
